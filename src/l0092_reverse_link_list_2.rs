7084a6717e9e3b219ea4f499ad3bd3f315599585
bai <nkbai@163.com>
2019-05-30 23:27:49 +0800

    反转链表，尤其是第二个，写的感觉很糟糕

---
+++ src/l0092_reverse_link_list_2.rs
@@ -0,0 +1,81 @@
+/*
+反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+
+说明:
+1 ≤ m ≤ n ≤ 链表长度。
+
+示例:
+
+输入: 1->2->3->4->5->NULL, m = 2, n = 4
+输出: 1->4->3->2->5->NULL
+
+这个解法反复不停的Box::new,应该可以不用new的,但是对于所有权和借用不是那么熟悉,只能先这么着.
+*/
+use crate::share::ListNode;
+struct Solution {}
+impl Solution {
+    pub fn reverse_between(head: Option<Box<ListNode>>, m: i32, n: i32) -> Option<Box<ListNode>> {
+        let mut nhead = head;
+        let mut rhead = nhead.as_mut();
+        let mut cnt = 1;
+        let mut result = Some(Box::new(ListNode::new(0)));
+        let mut rr = result.as_mut();
+        loop {
+            if cnt == m {
+                //开始反转
+                let mut start = Some(Box::new(ListNode::new(rhead.as_mut().unwrap().val)));
+                rhead = rhead.unwrap().next.as_mut();
+                cnt += 1;
+                loop {
+                    //题目应该确保链表长度大于n
+                    if cnt > n {
+                        break;
+                    }
+                    start = Some(Box::new(ListNode {
+                        val: rhead.as_mut().unwrap().val,
+                        next: start,
+                    }));
+
+                    rhead = rhead.unwrap().next.as_mut();
+                    cnt += 1;
+                }
+                let rstart = start.as_mut();
+                while start.is_some() {
+                    rr.as_mut().unwrap().next =
+                        Some(Box::new(ListNode::new(start.as_ref().unwrap().val)));
+                    rr = rr.unwrap().next.as_mut();
+                    start = start.unwrap().next.take();
+                }
+            }
+            if rhead.is_none() {
+                break;
+            }
+            rr.as_mut().unwrap().next = Some(Box::new(ListNode::new(rhead.as_ref().unwrap().val)));
+            rr = rr.unwrap().next.as_mut();
+            rhead = rhead.unwrap().next.as_mut();
+            cnt += 1;
+        }
+        result.unwrap().next
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::share::build_list_node;
+    #[test]
+    fn test_reverse() {
+        assert_eq!(
+            build_list_node(&vec![1, 4, 3, 2, 5]),
+            Solution::reverse_between(build_list_node(&vec![1, 2, 3, 4, 5]), 2, 4)
+        );
+        assert_eq!(
+            build_list_node(&vec![5, 4, 3, 2, 1]),
+            Solution::reverse_between(build_list_node(&vec![1, 2, 3, 4, 5]), 1, 5)
+        );
+        assert_eq!(
+            build_list_node(&vec![1, 2, 3, 4, 5]),
+            Solution::reverse_between(build_list_node(&vec![1, 2, 3, 4, 5]), 1, 1)
+        );
+    }
+}
---
+++ src/l206_reverse_linked_list.rs
@@ -0,0 +1,89 @@
+use crate::share::ListNode;
+struct Solution {}
+
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let result = None;
+        //        Solution::reverseListRecursive(head, result)
+        Solution::reverseListFor(head, result)
+    }
+    //    fn reverse_internal(head: &mut Option<Box<ListNode>>) -> retHelp {
+    //        if head.is_none() {
+    //            return retHelp {
+    //                cur: &mut None,
+    //                head: &mut None,
+    //            };
+    //        }
+    //        println!("enter :{:?}", head.as_ref().unwrap());
+    //        //传进来的head不可修改,只能创建新的
+    //        let mut cur = head;
+    //        let mut next = cur.as_mut().unwrap().next.take();
+    //        let mut result = Solution::reverse_internal(&mut next);
+    //        if result.cur.is_none() && result.head.is_none() {
+    //            //他的next是空,cur就是新head了
+    //            return retHelp {
+    //                cur: &mut None,
+    //                head: cur,
+    //            };
+    //        }else if result.cur.is_none() && result.head.is_some(){
+    //            return retHelp{
+    //                cur:
+    //            }
+    //        }
+    //        result.cur.as_mut().unwrap().next = *cur;
+    //        println!("return :{:?}", newhead);
+    //        &mut newhead.as_ref().unwrap().next
+    //    }
+    fn reverseListRecursive(
+        list: Option<Box<ListNode>>,
+        result: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        match list {
+            Some(val) => {
+                let result = Some(Box::new(ListNode {
+                    val: val.val, //通过复制Node的方式而不是我做的那种原地替换的方式
+                    next: result,
+                }));
+                Solution::reverseListRecursive(val.next, result)
+            }
+            Nil => result,
+        }
+    }
+    fn reverseListFor(
+        head: Option<Box<ListNode>>,
+        result: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut result2 = result;
+        let mut head2 = head;
+        loop {
+            match head2 {
+                Some(val) => {
+                    result2 = Some(Box::new(ListNode {
+                        val: val.val,
+                        next: result2,
+                    }));
+                    head2 = val.next;
+                }
+                None => break,
+            }
+        }
+
+        result2
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::share;
+    #[test]
+    fn test_reverse_list() {
+        let l1 = share::build_list_node(&vec![1, 2, 3, 4, 5]);
+        let l2 = share::build_list_node(&vec![5, 4, 3, 2, 1]);
+        assert_eq!(l1, Solution::reverse_list(l2));
+        assert_eq!(
+            share::build_list_node(&vec![1]),
+            Solution::reverse_list(share::build_list_node(&vec![1]))
+        )
+    }
+}
